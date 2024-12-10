6445f5a126d2f54d3314b600a48e449a817cc7af
Saif Ur Rehman <saifarshad3344@gmail.com>
2024-12-11 01:33:42 +0500

    feat : added apis for klarna stripe

--- src/app.js
+++ src/app.js
@@ -5,21 +5,22 @@ const http = require("http");
 require("dotenv").config();
 const { connectToDatabase } = require("../config/database.js");
 const routes = require("./routes");
+const Stripe = require('stripe');
 const fileUpload = require('express-fileupload');
 
 (async () => {
   connectToDatabase();
-  
+
   const allowedOrigins = "*";
   const corsOptionsAll = {
     optionsSuccessStatus: 202,
     origin: allowedOrigins,
     credentials: true,
   };
-  
+
   const app = express();
   const server = http.createServer(app);
-  
+  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
   app.use(fileUpload({
     useTempFiles: true,
     tempFileDir: '/tmp/'
@@ -48,7 +49,35 @@ const fileUpload = require('express-fileupload');
   });
 
   app.use("/api", routes);
+  app.post('/create-checkout-session', async (req, res) => {
+    const { serviceName, price } = req.body;
 
+    try {
+      const session = await stripe.checkout.sessions.create({
+        payment_method_types: ['klarna'],
+        line_items: [
+          {
+            price_data: {
+              currency: 'eur',
+              product_data: {
+                name: serviceName,
+              },
+              unit_amount: Math.round(price * 100),
+            },
+            quantity: 1,
+          },
+        ],
+        mode: 'payment',
+        success_url: `${process.env.FRONTEND_URL}/success`,
+        cancel_url: `${process.env.FRONTEND_URL}`,
+      });
+
+      res.json({ id: session.id });
+    } catch (error) {
+      console.error('Error creating checkout session:', error);
+      res.status(500).send('Internal Server Error');
+    }
+  });
   app.use((req, res) => {
     return res.status(404).send({ error: "Route not found" });
   });
